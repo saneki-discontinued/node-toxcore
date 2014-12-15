3145c6a423556305088ec52181a7b8a4df8d9f0f
saneki <fake.saneki@gmail.com>
2014-12-15 08:08:03 -0600

    Added a few async functions with tests, more jsdoc

--- test/tox.js
+++ test/tox.js
@@ -9,6 +9,36 @@ buffertools.extend(); // Extend Buffer.prototype
 describe('Tox', function() {
   var tox = new toxcore.Tox();
 
+  describe('#countFriendList()', function() {
+    it('should return 0 in callback when no friend', function(done) {
+      tox.countFriendList(function(err, res) {
+        res.should.equal(0);
+        done(err);
+      });
+    });
+  });
+
+  describe('#countFriendListSync()', function() {
+    it('should return 0 when no friends', function() {
+      tox.countFriendListSync().should.equal(0);
+    });
+  });
+
+  describe('#hasFriend()', function() {
+    it('should return false in callback if not added', function(done) {
+      tox.hasFriend(0, function(err, res) {
+        res.should.be.false;
+        done(err);
+      });
+    });
+  });
+
+  describe('#hasFriendSync()', function() {
+    it('should return false if not added', function() {
+      tox.hasFriendSync(0).should.be.false;
+    });
+  });
+
   describe('#hasHandle()', function() {
     it('should return true if a tox handle is present', function() {
       tox.hasHandle().should.be.true;
@@ -24,6 +54,24 @@ describe('Tox', function() {
 
     it('should correctly hash a String', function() {
       tox.hashSync('abc').toHex().toLowerCase().should.equal(abcHashed);
+    });
+  });
+
+  describe('#kill()', function() {
+    it('should clear handle after killing', function(done) {
+      var temp = new toxcore.Tox();
+      temp.kill(function(err) {
+        temp.hasHandle().should.not.exist;
+        done(err);
+      });
+    });
+  });
+
+  describe('#killSync()', function() {
+    it('should clear handle after killing', function() {
+      var temp = new toxcore.Tox();
+      temp.killSync();
+      temp.hasHandle().should.not.exist;
     });
   });
 
