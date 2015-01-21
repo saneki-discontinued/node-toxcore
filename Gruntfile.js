371f1ea59810fed66d64a4c129b614809951a5e1
saneki <fake.saneki@gmail.com>
2015-01-21 17:01:37 -0600

    Generate doc for toxdns/toxencryptsave

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jsdoc: {
       dist: {
-        src: ['lib/tox.js', 'lib/events.js'],
+        src: ['lib/tox.js', 'lib/toxdns.js', 'lib/toxencryptsave.js', 'lib/events.js'],
         options: {
           destination: 'doc'
         }
