1de1a9eb3b8b311e699424d14cd967828ae0635f
saneki <s@neki.me>
2015-03-18 15:24:50 -0500

    Added Tox#bootstrap, bootstrap error handling, new_api examples, updated save function names for latest tox.h

---
+++ examples/bin/new/promises-example.js
@@ -0,0 +1,52 @@
+/**
+ * node-toxcore (new_api) async example with bluebird promises.
+ */
+
+var async = require('async');
+var Promise = require('bluebird');
+var toxcore = require('toxcore').new;
+
+Promise.promisifyAll(async);
+Promise.promisifyAll(toxcore);
+
+var tox = new toxcore.Tox();
+
+/**
+ * Bootstrap tox via hardcoded nodes.
+ * For more nodes, see: https://wiki.tox.im/Nodes
+ */
+var bootstrap = function(callback) {
+  // Define nodes to bootstrap from
+  var nodes = [
+    { maintainer: 'Impyy',
+      address: '178.62.250.138',
+      port: 33445,
+      key: '788236D34978D1D5BD822F0A5BEBD2C53C64CC31CD3149350EE27D4D9A2F9B6B' },
+    { maintainer: 'sonOfRa',
+      address: '144.76.60.215',
+      port: 0,
+      key: '04119E835DF3E78BACF0F84235B300546AF8B936F035185E2A8E9E0A67C8924F' }
+  ];
+
+  async.mapAsync(nodes, function(node, cb) {
+    tox.bootstrapAsync(node.address, node.port, node.key).then(function() {
+      console.log('Successfully bootstrapped from ' + node.maintainer + ' at ' + node.address + ':' + node.port);
+      console.log('... with key ' + node.key);
+      cb();
+    }).catch(function(err) {
+      console.error('Error bootstrapping from ' + node.maintainer + ':', err);
+      cb(err);
+    });
+  }).then(function() {
+    // Once all nodes have been bootstrapped from, call our callback
+    callback();
+  });
+};
+
+// Initialize everything + bootstrap from nodes, then when everything
+// is ready, start
+async.parallel([
+  bootstrap       // Bootstrap
+], function() {
+  tox.start(); // Start
+});
---
+++ examples/bin/new/sync-example.js
@@ -0,0 +1,26 @@
+/**
+ * A tiny tox bot example using node-toxcore's synchronous methods (new_api).
+ */
+
+var toxcore = require('toxcore').new;
+var tox = new toxcore.Tox();
+
+// Specify nodes to bootstrap from
+var nodes = [
+  { maintainer: 'Impyy',
+    address: '178.62.250.138',
+    port: 33445,
+    key: '788236D34978D1D5BD822F0A5BEBD2C53C64CC31CD3149350EE27D4D9A2F9B6B' },
+  { maintainer: 'sonOfRa',
+    address: '144.76.60.215',
+    port: 33445,
+    key: '04119E835DF3E78BACF0F84235B300546AF8B936F035185E2A8E9E0A67C8924F' }
+];
+
+// Bootstrap from nodes
+nodes.forEach(function(node) {
+  tox.bootstrapSync(node.address, node.port, node.key);
+});
+
+// Start the tox_iterate loop
+tox.start();
