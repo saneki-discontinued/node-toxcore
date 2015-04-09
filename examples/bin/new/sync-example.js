c1380a147e0118dafa613a07ab7d0c3aa33574a1
saneki <s@neki.me>
2015-04-08 20:47:34 -0500

    Added (mostly tested) friend events/callbacks

--- examples/bin/new/sync-example.js
+++ examples/bin/new/sync-example.js
@@ -20,11 +20,53 @@ var nodes = [
 // Bootstrap from nodes
 nodes.forEach(function(node) {
   tox.bootstrapSync(node.address, node.port, node.key);
+  console.log('Successfully bootstrapped from ' + node.maintainer + ' at ' + node.address + ':' + node.port);
+  console.log('... with key ' + node.key);
 });
 
 tox.on('selfConnectionStatus', function(e) {
   console.log(e.isConnected() ? 'Connected' : 'Disconnected');
 });
+
+tox.on('friendName', function(e) {
+  console.log('Friend[' + e.friend() + '] changed their name: ' + e.name());
+});
+
+tox.on('friendStatusMessage', function(e) {
+  console.log('Friend[' + e.friend() + '] changed their status message: ' + e.statusMessage());
+});
+
+tox.on('friendStatus', function(e) {
+  console.log('Friend[' + e.friend() + '] changed their status: ' + e.status());
+});
+
+tox.on('friendConnectionStatus', function(e) {
+  console.log('Friend[' + e.friend() + '] is now ' + (e.isConnected() ? 'online' : 'offline'));
+});
+
+tox.on('friendTyping', function(e) {
+  console.log('Friend[' + e.friend() + '] is ' + (e.isTyping() ? 'typing' : 'not typing'));
+});
+
+tox.on('friendReadReceipt', function(e) {
+  console.log('Friend[' + e.friend() + '] receipt: ' + e.receipt());
+});
+
+tox.on('friendRequest', function(e) {
+  tox.addFriendNoRequestSync(e.publicKey());
+  console.log('Received friend request: ' + e.message());
+  console.log('Accepted friend request from ' + e.publicKeyHex());
+});
+
+tox.on('friendMessage', function(e) {
+  if(e.isAction()) {
+    console.log('** Friend[' + e.friend() + '] ' + e.message() + ' **');
+  } else {
+    console.log('Friend[' + e.friend() + ']: ' + e.message());
+  }
+});
+
+console.log('Address: ' + tox.getAddressHexSync());
 
 // Start the tox_iterate loop
 tox.start();
