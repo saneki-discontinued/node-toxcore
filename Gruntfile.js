9bfa8ba5ebae8b8f6c1ce4afb9791a7d8c3a6802
saneki <s@neki.me>
2015-04-18 17:35:26 -0500

    Updated gruntfile to build documentation with new theme, fixed jsdoc callback info in tox.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,16 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jsdoc: {
       dist: {
-        src: ['lib/tox.js', 'lib/toxdns.js', 'lib/toxencryptsave.js', 'lib/events.js'],
+        src: [
+          'lib/events.js',
+          'lib/tox.js',
+          'lib/toxoptions.js',
+          'lib/tox_old.js'
+        ],
         options: {
-          destination: 'doc'
+          destination: 'doc',
+          template : 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
+          configure : 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json'
         }
       }
     }
