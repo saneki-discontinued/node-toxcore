724ecef453dc7c2f99a76ca1dfe6b5145d052717
saneki <s@neki.me>
2015-08-15 16:23:47 -0500

    Added ToxClient#publicKey(), secretKey() with tests

--- test/toxclient.js
+++ test/toxclient.js
@@ -5,13 +5,34 @@ var ToxClient = require(path.join(__dirname, '..', 'lib', 'hla', 'toxclient'));
 
 // Copied from test/tox.js, make more portable?
 var addressRegex = /^[0-9a-fA-F]{76}$/;
+var keyRegex = /^[0-9a-fA-F]{64}$/;
 
 describe('ToxClient', function() {
   var client = new ToxClient();
 
   describe('#address()', function() {
-    it('should return the address as an upper-case string', function() {
+    it('should return the address as an upper-case hex string', function() {
       client.address().should.match(addressRegex);
+    });
+  });
+
+  describe('#publicKey()', function() {
+    it('should return the public key as an upper-case hex string', function() {
+      client.publicKey().should.match(keyRegex);
+    });
+
+    it('should be contained within the address', function() {
+      client.address().indexOf(client.publicKey()).should.equal(0);
+    });
+  });
+
+  describe('#secretKey()', function() {
+    it('should return the secret key as an upper-case hex string', function() {
+      client.secretKey().should.match(keyRegex);
+    });
+
+    it('should not equal the public key', function() {
+      client.secretKey().should.not.equal(client.publicKey());
     });
   });
 
