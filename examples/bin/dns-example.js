0074f84aa4cf9cc14871b987868b7ab16802a178
saneki <s@neki.me>
2015-09-30 17:16:03 -0500

    Some documentation for dns-example.js

--- examples/bin/dns-example.js
+++ examples/bin/dns-example.js
@@ -22,22 +22,35 @@ var clients = {
   'toxme.io': undefined // toxme.io is the default if no key provided
 };
 
+// Create clients
 Object.keys(clients).map(function(value, index) {
   clients[value] = new toxcore.ToxDns({ key: clients[value] });
 });
 
+/**
+ * Gets the domain of a ToxDns address.
+ * @param {String} dnsaddr - Address (ex. 'user@toxme.io')
+ * @return {String} domain name, or undefined if none (bad address format)
+ */
 var getAddressDomain = function(dnsaddr) {
   var split = dnsaddr.split('@');
   if(split.length === 2)
     return split[1];
 };
 
+/**
+ * Get the ToxDns client for a specific address.
+ * @param {String} dnsaddr - Address (ex. 'user@toxme.io')
+ * @return {ToxDns} client to use for the given address, or undefined if none
+ *                  for the address domain
+ */
 var getClientForAddress = function(dnsaddr) {
   var domain = getAddressDomain(dnsaddr);
   if(domain !== undefined && clients[domain] !== undefined)
     return clients[domain];
 };
 
+// Resolve each ToxDns in argv and output the results
 process.argv.slice(2).forEach(function(dnsaddr) {
   var client = getClientForAddress(dnsaddr);
   client.resolveHex(dnsaddr, function(err, addr) {
